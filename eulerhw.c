de64ff29e3db5d53cdcf935284b94c5f903abf48
Asim Kapparova <akapparova@stuy.edu>
2017-09-17 23:39:43 -0400

    finished euler homework

--- eulerhw.c
+++ eulerhw.c
@@ -3,9 +3,22 @@
 
 int main (){
   int ans1 = 0;
-  int ans6 = 0;
+  int counter1;
+  for (counter1 = 0; counter1 < 1000; counter1 ++){
+    if ((counter1 % 3 == 0) || (counter1 % 5 == 0)){
+      ans1 += counter1;
+    }
+  }
 
-  printf("The answer to problem #1 is %d", ans1);
-  printf("The answer to problem #6 is %d", ans6);
+  int ans6a = 0;
+  int ans6b = 0;
+  int counter6;
+  for (counter6 = 1; counter6 <= 100; counter6 ++){
+    ans6a += (counter6 * counter6);
+    ans6b += counter6;
+  }
+  int ans6 = -1 * (ans6a - (ans6b * ans6b));
 
+  printf("The answer to problem #1 is %d (should be 233168)\n", ans1);
+  printf("The answer to problem #6 is %d (should be 25164150)\n", ans6);
 }
