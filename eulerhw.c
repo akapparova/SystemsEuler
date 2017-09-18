ff61333033668e8128d2f86f76c593a83306536b
Asim Kapparova <akapparova@stuy.edu>
2017-09-17 23:02:39 -0400

    starting fr

--- eulerhw.c
+++ eulerhw.c
@@ -1,1 +1,11 @@
-int main (){}
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+  int ans1 = 0;
+  int ans6 = 0;
+
+  printf("The answer to problem #1 is %d", ans1);
+  printf("The answer to problem #6 is %d", ans6);
+
+}
